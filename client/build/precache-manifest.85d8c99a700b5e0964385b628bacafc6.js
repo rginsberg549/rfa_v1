3da8dbcaec87ab39b30bed28b27c2fea7ba55372
rginsberg <rginsberg549@gmail.com>
2020-12-15 21:21:16 -0800

    removing random code in server.js file

---
+++ client/build/precache-manifest.85d8c99a700b5e0964385b628bacafc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb20f37c3fae94e5a87e8188ec7780f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ff7c5cc62449410c498",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "72814fd3a076d3dc3a51",
+    "url": "/static/js/2.8e091537.chunk.js"
+  },
+  {
+    "revision": "f2508b720fa7494d752730597a65b748",
+    "url": "/static/js/2.8e091537.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756125c8cc914f04c749",
+    "url": "/static/js/3.5951774e.chunk.js"
+  },
+  {
+    "revision": "3ff7c5cc62449410c498",
+    "url": "/static/js/main.df2ad5e6.chunk.js"
+  },
+  {
+    "revision": "a3efd522590600a4abe1",
+    "url": "/static/js/runtime-main.999ec734.js"
+  }
+]);
